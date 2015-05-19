ac1284988229e8a708b719a7ef4e957275bd030d
chunhui.yao <chunhui.yao@zentertain.net>
2015-05-19 20:18:33 +0800

    prevent warning

--- extensions/ccb-reader/CCBAnimationManager.js
+++ extensions/ccb-reader/CCBAnimationManager.js
@@ -588,8 +588,8 @@ cc.BuilderAnimationManager = cc.Class.extend({
         if (keyframes.length === 0) {
             // Use base value (no animation)
             var baseValue = this._getBaseValue(node, seqProp.getName());
-            if(!baseValue)
-                cc.log("cc.BuilderAnimationManager._setFirstFrame(): No baseValue found for property");
+            //if(!baseValue)
+            //    cc.log("cc.BuilderAnimationManager._setFirstFrame(): No baseValue found for property");
             this._setAnimatedProperty(seqProp.getName(), node, baseValue, tweenDuration);
         } else {
             // Use first keyframe
